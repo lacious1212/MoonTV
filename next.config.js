ef0d2d6b57d584d5b4276914999fa5f533ac8b3c
lacious1212 <138612043+lacious1212@users.noreply.github.com>
2026-05-19 13:22:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,48 @@ const nextConfig = {
   eslint: {
     dirs: ['src'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img1.doubanio.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img2.doubanio.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img3.doubanio.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img9.doubanio.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.weserv.nl',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/zh',
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'https://api.douban.com/v2/:path*',
+      },
+    ];
+  },
+  // 💡 下面這段就是我們加進去的安全通道設定
   env: {
     NEXT_PUBLIC_DOUBAN_PROXY_TYPE: 'cors-proxy-zwei',
     NEXT_PUBLIC_DOUBAN_IMAGE_PROXY_TYPE: 'cmliussss-cdn-tencent',
