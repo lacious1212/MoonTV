4e65dc6e16b1303bb6c828d635b046ce53fc2ecf
lacious1212 <138612043+lacious1212@users.noreply.github.com>
2026-05-19 13:17:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  eslint: {
+    dirs: ['src'],
+  },
   env: {
     NEXT_PUBLIC_DOUBAN_PROXY_TYPE: 'cors-proxy-zwei',
     NEXT_PUBLIC_DOUBAN_IMAGE_PROXY_TYPE: 'cmliussss-cdn-tencent',
